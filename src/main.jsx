cdad6786e130d6e6772a95514797611bafd9cf8d
priyanshaairtel <priyansha.gupta@airtel.com>
2026-05-07 16:02:48 +0530

    Create main.jsx
    Add main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OEWorkbenchV3 from './OEWorkbench.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OEWorkbenchV3 />
+  </React.StrictMode>,
+)
